75c8a7629cdf2100ee3e66dd82b74bdb8af879de
DanieleNegro <danieleweb1987@gmail.com>
2024-09-19 09:33:46 +0200

    Added Deborah Kurata's products

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import data from './products2.js';
+import data from './products3.js';
 
 const app = express();
 
 app.get('/api/products', (req, res) => {
+  console.log('/api/products');
   return res.send(data.products);
 });
 
 app.get('/api/products/:id', (req, res) => {
+  console.log(`/api/products/${+req.params.id}`);
   const product = data.products.find((item) => item.id === +req.params.id);
   return res.send(product);
 });
