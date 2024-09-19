0a11b2d58c0d5485cb561d3b4854e1157b24d461
DanieleNegro <danieleweb1987@gmail.com>
2024-09-19 14:53:42 +0200

    add json body parser

--- index.js
+++ index.js
@@ -3,15 +3,43 @@ import data from './products3.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/api/products', (req, res) => {
-  console.log('/api/products');
+  console.log('GET: /api/products');
   return res.send(data.products);
 });
 
 app.get('/api/products/:id', (req, res) => {
-  console.log(`/api/products/${+req.params.id}`);
-  const product = data.products.find((item) => item.id === +req.params.id);
+  const productId = +req.params.id;
+  const product = data.products.find((item) => item.id === productId);
+  console.log(`GET: /api/products/${productId}`, product);
   return res.send(product);
+});
+
+app.post('/api/products', (req, res) => {
+  const product = req.body;
+  console.log('POST: /api/products', product);
+  product.id = data.products.length + 1;
+  data.products.push(product);
+  return res.send(product);
+});
+
+app.delete('/api/products/:id', (req, res) => {
+  const productId = +req.params.id;
+  console.log(`DELETE: /api/products/${productId}`);
+  const filteredList = data.products.filter((item) => item.id !== productId);
+  data.products = filteredList;
+  return res.send(filteredList);
+});
+
+app.put('/api/products/:id', (req, res) => {
+  const productId = +req.params.id;
+  const product = req.body;
+  console.log(`PUT: /api/products/${productId}`, product);
+  const index = data.products.findIndex((item) => item.id === productId);
+  data.products[index] = product;
+  return res.send(data.products[index]);
 });
 
 app.listen(8081, () => console.log('Server listening on port 8081!'));
